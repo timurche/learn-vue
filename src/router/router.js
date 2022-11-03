8d8b474516363e3f1601713fa705b061a275a30f
timurche <63548007+timurche@users.noreply.github.com>
2022-11-03 10:16:54 +0500

    router

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,8 @@
 import Main from "@/pages/Main";
 import postsPage from "@/pages/postsPage";
-import aboutUs from "@/pages/aboutUs";
 import singlePost from "@/pages/singlePost";
 import { createRouter, createWebHistory } from "vue-router";
-
+import aboutUs from "@/pages/aboutUs";
 const routes = [
   {
     path: "/",
