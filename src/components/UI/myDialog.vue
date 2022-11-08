bd06f05a4ee4d2ed5f99306487253c0b3af68ea9
timurche <63548007+timurche@users.noreply.github.com>
2022-11-08 16:45:52 +0500

    mixins

--- src/components/UI/myDialog.vue
+++ src/components/UI/myDialog.vue
@@ -7,19 +7,11 @@
 </template>
 
 <script>
+import toggleMixin from "@/mixins/toggleMixin";
+import helloMixin from "@/mixins/helloMixin";
 export default {
   name: "my-dialog",
-  props: {
-    show: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  methods: {
-    hideDialog() {
-      this.$emit("update:show", false);
-    },
-  },
+  mixins: [toggleMixin, helloMixin],
 };
 </script>
 
---
+++ src/mixins/helloMixin.js
@@ -0,0 +1,5 @@
+export default {
+  created() {
+    alert("Pidori idut!!!");
+  }
+};
