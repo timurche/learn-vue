cddbe37649723330cf1ebb4d9080e989f3966f1e
timurche <63548007+timurche@users.noreply.github.com>
2022-11-11 17:01:44 +0500

    vux

--- src/pages/store.vue
+++ src/pages/store.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>fffff</div>
+  <div>
+    <div>
+      <h3>
+        {{ $store.state.isAuth ? "Авторизован" : "Чужак" }}
+      </h3>
+      <my-button
+        v-if="$store.state.isAuth == false"
+        @click="$store.commit('authNow')"
+        >Войти</my-button
+      >
+    </div>
+
+    <div>
+      {{ $store.getters.likesView }}
+      <my-button @click="$store.commit('incLike')"> 👍 </my-button>
+      <my-button @click="$store.commit('decLike')"> 👎 </my-button>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,24 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    likes: 0
+    likes: 0,
+    isAuth: false
   },
-  getters: {},
-  mutations: {},
+  getters: {
+    likesView(state) {
+      return state.likes + " likes";
+    }
+  },
+  mutations: {
+    incLike(state) {
+      state.likes += 1;
+    },
+    decLike(state) {
+      state.likes -= 1;
+    },
+    authNow(state) {
+      state.isAuth = true;
+    }
+  },
   actions: {}
 });
